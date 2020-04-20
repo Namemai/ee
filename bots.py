0d2a41d8892a8368f80c0f0c88335dacc908cb5b
Namemai <40319864+Namemai@users.noreply.github.com>
2020-04-20 23:13:49 +0700

    Update bots.py

--- bots.py
+++ bots.py
@@ -4,6 +4,6 @@ import threading
 def login(resp, auth):
     bot = OverPoll(resp, auth)
 
-threading.Thread(target=login, args=("client1","token")).start()
-threading.Thread(target=login, args=("client2","token")).start()
+threading.Thread(target=login, args=("client1","EQ8CpzzqMhIRQax4m69e.tsWh/XhwZNQR/EYas4X8FG.Fv5f/rf+lNnH1u/We0D6FHv0m81QAYP8Qvvo2Xyj8AE=")).start()
+threading.Thread(target=login, args=("client2","EQtBjlsLhyYE3AhQtVUd.ARAzAuWFaouOQmcAei0odq.HJtHiHbQTiMoJC/IQdXVoCXxoN80CqDZSw/VJkNpBo0=")).start()
 print("USER LOGIN SUCCES.!!")
