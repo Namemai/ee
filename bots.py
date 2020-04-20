7cc4bcd5cac13dc32a169c70a0985aa1efb848fc
Namemai <40319864+Namemai@users.noreply.github.com>
2020-04-20 23:00:26 +0700

    Update bots.py

--- bots.py
+++ bots.py
@@ -6,5 +6,4 @@ def login(resp, auth):
 
 threading.Thread(target=login, args=("client1","token")).start()
 threading.Thread(target=login, args=("client2","token")).start()
-threading.Thread(target=login, args=("client3","token")).start()
 print("USER LOGIN SUCCES.!!")
